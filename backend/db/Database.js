c12cb0240ace888bbc1fb4f921a8bd5bd0c0c76a
chilunhay <72005042+chilunhay@users.noreply.github.com>
2023-02-24 11:24:49 +0700

    fix db

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", false);
+
 const connectDatabase = () => {
     mongoose.connect(process.env.DB_URL, {
         useNewUrlParser: true,
